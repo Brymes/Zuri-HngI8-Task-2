4199326c182c83ef7b450f979f1268bcd96f4824
TImothy Odebunmi <dev.brymes@gmail.com>
2021-08-20 15:16:11 +0100

    Removed Flask-Restul Structure

--- api/resources.py
+++
@@ -1,6 +0,0 @@
-from flask_restful import Resource
-
-
-class Index(Resource):
-    def get(self):
-        return "Hello, World!"
--- api/routes.py
+++
@@ -1,5 +0,0 @@
-from api.resources import Index
-
-
-def initialize_routes(api):
-    api.add_resource(Index, '/')
